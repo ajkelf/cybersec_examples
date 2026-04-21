6924f056ce1c28f9aedf317bdda45e7ee4a0d7f4
dbelyavskiy <db@tehnesis.com>
2026-04-21 13:12:32 +0300

    SSRF example

--- ssrf/index.php
+++ ssrf/index.php
@@ -11,7 +11,7 @@ else {
 <p><a href='/ssrf/?image=file:///home/projects/hse2/ssrf/lights_at_the_sea.jpg' target="_blank">Применение SSRF - локальный файл</a></p>
 <p><a href='/ssrf/?image=https://www.hse.ru/pubs/share/direct/1132084150.jpg' target="_blank">Применение SSRF - remote файл</a></p>
 <hr>
-<p><a href='/ssrf/?image=file://file:///etc/resolv.conf' target="_blank">Применение SSRF - файл конфигурации DNS</a></p>
-<p><a href='/ssrf/?image=file://file:///etc/shadow' target="_blank">Применение SSRF - файл shadow</a></p>
+<p><a href='/ssrf/?image=file:///etc/resolv.conf' target="_blank">Применение SSRF - файл конфигурации DNS</a></p>
+<p><a href='/ssrf/?image=file:///etc/shadow' target="_blank">Применение SSRF - файл shadow</a></p>
 <?php
 }
