0f34064f17c2b5b4e2e66adedc7bf6c0735e9b55
dbelyavskiy <db@tehnesis.com>
2026-04-21 13:40:44 +0300

    CSRF example

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Examples for cybersecurity lessons:
 
 1) /hardtobreak/ - HTML form for brute force
 2) /search/ - XSS example
-3) /ssrf/ - SSRT example
+3) /ssrf/ - SSRF example
+4) /csrf/ - CSRF example
---
+++ csrf/index.php
@@ -0,0 +1,5 @@
+<form action='/hardtobreak/' method='post'>
+  <input type="hidden" name="username" value="busyuser">
+  <input type="hidden" name="password" value="sunshine">
+  <input type="submit" value="Log in">
+</form>
